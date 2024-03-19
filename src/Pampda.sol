f46bc40de752d76a7a0dd3240140b7785023da5c
cgccld <tasibii.web3@gmail.com>
2024-03-19 13:49:21 +0700

    add PAMP & redeploy

---
+++ src/Pampda.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Pampda is ERC20 {
+  constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
+
+  function mint(address to_, uint256 amount_) public {
+    _mint(to_, amount_);
+  }
+  
+  function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+    address owner = _msgSender();
+    _approve(owner, spender, allowance(owner, spender) + addedValue);
+    return true;
+  }
+
+  function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+    address owner = _msgSender();
+    uint256 currentAllowance = allowance(owner, spender);
+    require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+    unchecked {
+      _approve(owner, spender, currentAllowance - subtractedValue);
+    }
+    return true;
+  }
+}
